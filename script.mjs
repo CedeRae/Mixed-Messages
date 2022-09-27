0eac57f05794ec4ae577c2eb38eaf4e5d624f1b8
Cede Rae <cederae@gmail.com>
2022-09-27 16:32:52 -0700

    Moved script to html doc. Quotes print to screen.

--- script.mjs
+++ script.mjs
@@ -1,24 +1,3 @@
 
-//import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
-const getQuote = () => {
-    const api_url = 'https://type.fit/api/quotes';
-
-    fetch(api_url).then((resp) => {
-        if (!resp.ok) {
-            throw Error("Error");
-        }
-        return resp.json();
-    })
-    .then(data => {
-        const html = data.map(quote => {
-            return `${quote[0].text} - ${quote[0].author}`
-        })
-        document.querySelector('#quote').insertAdjacentHTML("afterbegin", `<p>${html}</p>`);
-    })
-    .catch(error => {
-        console.log(error);
-    })
-};
-
-getQuote();
